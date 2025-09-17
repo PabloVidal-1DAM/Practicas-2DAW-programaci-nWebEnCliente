bce97c830cb7c9e8b65f828e302b3a1b60e41fae
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-09-17 18:59:28 +0200

    hola

--- PracticasPabloV_PEC/Practica2.02_PabloV/Ejercicio2/biblioteca.js
+++ PracticasPabloV_PEC/Practica2.02_PabloV/Ejercicio2/biblioteca.js
@@ -1,6 +1,8 @@
+// Ejercicio 2: Sólo Números
 function soloNumeros(numero){
-    if (isNaN(numero)){
+    if (isNaN(numero)){ // comprueba si el usuario ha pasado un número
         console.log("Únicamente se aceptan valores númericos")
+        return;
     }
     analisisNumerico(numero);
 }
@@ -9,6 +11,25 @@ function analisisNumerico(numero){
     console.log("EL numero " +numero+ "es: "(numero % 2 == 0 ? "par" : "impar"));
 
     // Comprobación de número negativo o positivo
+    console.log("Es: "(numero >=0 ? "positivo" : "negativo"));
+
+    //Comprobación de número primo
+    let primo = false;
+    let contadorPrimo = 0;
+
+    for (let i = 1; i<= numero; i++){
+        if(numero % i == 0){
+            contadorPrimo++;
+        }
+    }
+
+    if (contadorPrimo === 2){
+        primo = true;
+    }
+
+    console.log("Es primo?: " + primo)
+
+
 }
 
 export {soloNumeros, analisisNumerico};
