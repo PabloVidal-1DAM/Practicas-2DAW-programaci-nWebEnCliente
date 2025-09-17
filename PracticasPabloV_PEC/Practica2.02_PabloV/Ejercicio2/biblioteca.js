f6f2a5957bd29ce9d633a156e007022045bacb66
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-09-17 16:54:52 +0200

    hola

--- PracticasPabloV_PEC/Practica2.02_PabloV/Ejercicio2/biblioteca.js
+++ PracticasPabloV_PEC/Practica2.02_PabloV/Ejercicio2/biblioteca.js
@@ -2,8 +2,13 @@ function soloNumeros(numero){
     if (isNaN(numero)){
         console.log("Únicamente se aceptan valores númericos")
     }
+    analisisNumerico(numero);
 }
 function analisisNumerico(numero){
-    // comprobación de par/impar:
+    // Comprobación de par/impar:
     console.log("EL numero " +numero+ "es: "(numero % 2 == 0 ? "par" : "impar"));
+
+    // Comprobación de número negativo o positivo
 }
+
+export {soloNumeros, analisisNumerico};
