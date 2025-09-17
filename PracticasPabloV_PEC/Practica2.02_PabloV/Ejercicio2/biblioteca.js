7ca479218fa265fc66f7b923db0eed3d1c4ce8d7
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-09-17 16:03:39 +0200

    hola

--- PracticasPabloV_PEC/Practica2.02_PabloV/Ejercicio2/biblioteca.js
+++ PracticasPabloV_PEC/Practica2.02_PabloV/Ejercicio2/biblioteca.js
@@ -3,3 +3,7 @@ function soloNumeros(numero){
         console.log("Únicamente se aceptan valores númericos")
     }
 }
+function analisisNumerico(numero){
+    // comprobación de par/impar:
+    console.log("EL numero " +numero+ "es: "(numero % 2 == 0 ? "par" : "impar"));
+}
