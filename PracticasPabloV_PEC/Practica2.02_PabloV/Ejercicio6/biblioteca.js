50b51674445a7a0b3b4642109be8336c11d972ab
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-09-18 19:05:05 +0200

    hola

--- PracticasPabloV_PEC/Practica2.02_PabloV/Ejercicio6/biblioteca.js
+++ PracticasPabloV_PEC/Practica2.02_PabloV/Ejercicio6/biblioteca.js
@@ -14,19 +14,25 @@ function calculadora(numero1, numero2, operador){
     }else if(operador === "/" ){
         dividir(numero1, numero2);
     }else{
-        console.log("No se aceptarán operadores que no sean: Sumar, Restar, Multiplicar o Dividir.")
+        console.log("No se aceptarán operadores que no sean: Sumar, Restar, Multiplicar o Dividir.");
         return;
     }
 }
 function sumar(numero1, numero2){
-
+    let resultado = numero1 + numero2;
+    return resultado;
 }
 function restar(numero1, numero2){
+    let resultado = numero1 - numero2;
+    return resultado;
     
 }
 function dividir(numero1, numero2){
-    
+    let resultado = numero1 / numero2;
+    return resultado;
 }
 function multiplicar(numero1,numero2){
-
+    let resultado = numero1 * numero2;
+    return resultado;
 }
+export {calculadora ,sumar, restar, dividir, multiplicar};
