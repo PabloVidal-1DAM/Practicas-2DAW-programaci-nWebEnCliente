107f7f12f5591f701585a1324cedf514283fad6e
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-09-19 16:00:16 +0200

    hola

---
+++ PracticasPabloV_PEC/practica-react/src/components/Contenedor.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Contenedor.css";
+
+const Contenedor= () => {
+    //Codigo en Javascript
+
+    return (
+        <>
+        <div className="contenedor_css">
+
+        </div>
+        </>
+    );
+};
