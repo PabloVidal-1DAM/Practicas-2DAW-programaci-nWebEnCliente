95e01d7a967ac15094bb5ff68d9d5ed009ef847e
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-09-22 11:58:11 +0200

    22/09/2025

--- PracticasPabloV_PEC/practica-react/src/components/Ejercicio3/Pelicula.jsx
+++ PracticasPabloV_PEC/practica-react/src/components/Ejercicio3/Pelicula.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Pelicula.css";
+import Contenedor from "../Ejercicio1/Contenedor";
 
 const Pelicula = (props) =>{
 
@@ -7,6 +8,10 @@ const Pelicula = (props) =>{
         <>
             <h2>{props.titulo}</h2>
             <p>Dirigida por: {props.direccion}</p>
+            <p>cartela: {props.cartela}</p>
+            <p>{props.children}</p>
+            <Contenedor></Contenedor>
         </>
     );
-};export default Pelicula;
+};
+export default Pelicula;
