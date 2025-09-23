6992659f02bd71a1f6b0906fb188d37e5b0bc913
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-09-23 22:26:36 +0200

    23/09/2025

--- PracticasPabloV_PEC/Practica2.03-PabloV/ejercicio1.js
+++ PracticasPabloV_PEC/Practica2.03-PabloV/ejercicio1.js
@@ -1,20 +1,30 @@
+"use strict";
 //Ejercicio 1: Constructor de Objetos
-const crearCurso =(nombre, anio, descripcion,alumnado) =>{
+
+ /* si a descripción no se le pasa nada, pondra "por defecto" al incializarlo en el constructor, y este
+ constructor se vuelve a usar para inicializar alumnado como un aray vacío, como pide el ejercicio*/
+const crearCurso =(nombre, anio, descripcion = "Por defecto",alumnado = []) =>{
+
+    /* función callback que recorrerá alumnado para ver si todo lo que contiene son Strings*/
+    let verificarCadena = alumnado.every((valor,indice,array) => typeof valor === "string")
+
+    if (!verificarCadena){
+        console.log("Unicamente se deben pasar cadenas de texto al array de alumnos")
+        alumnado = undefined;
+    }
+
+    if(!isNaN(nombre)){
+        console.log("Unicamente se acepta texto para el nombre del curso")
+        nombre = undefined;
+    }
+
+
     return{
         nombreCurso: nombre,
         anio: anio,
-       /* if (descripcion.existeValor(descripcion)){
-
-        }*/
-        descripcion: "Por defecto",
+        descripcion: descripcion,
         alumnado: [...alumnado]
     };
 }
 
-const existeValor = (valor) =>{
-    if (valor.length == null){
-        return true;
-    }else{
-        return false;
-    }
-}
+export {crearCurso};
---
+++ PracticasPabloV_PEC/Practica2.03-PabloV/main.js
@@ -0,0 +1,3 @@
+import {crearCurso} from "./ejercicio1.js"
+
+console.log(crearCurso("Desarrollo Aplicaciones Web", 2005, "Curso para ser desarrollador web.", ["juan", "Maria", "Pepe", "jimenez"]))
