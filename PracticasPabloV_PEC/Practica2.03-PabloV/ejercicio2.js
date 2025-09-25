e77fa2d9c7f0f3d5e90a32265599460fa5a7107a
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-09-25 19:00:00 +0200

    25/09/2025

--- PracticasPabloV_PEC/Practica2.03-PabloV/ejercicio2.js
+++ PracticasPabloV_PEC/Practica2.03-PabloV/ejercicio2.js
@@ -1,7 +1,7 @@
 "use strict";
 //Ejercicio2: Mostrando objetos.
 const recorrerObjeto = (objeto) =>{
-    //  he usado un for in ya que solo tengo que recorrer el objeto, mostrando el clave-valor de cada atributo del objeto que se pase como parámetro
+    //  for in que recorre el objeto, mostrando el clave-valor de cada atributo del objeto que se pase como parámetro
     for (let clave in objeto){
         console.log(clave+ ": "+objeto[clave]);
     };
