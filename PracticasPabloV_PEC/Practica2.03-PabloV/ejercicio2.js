159da25a0dd600c480f17c525ea53bb0ebf7ad5a
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-09-26 15:58:36 +0200

    26/09/2025

--- PracticasPabloV_PEC/Practica2.03-PabloV/ejercicio2.js
+++ PracticasPabloV_PEC/Practica2.03-PabloV/ejercicio2.js
@@ -2,8 +2,10 @@
 //Ejercicio2: Mostrando objetos.
 const recorrerObjeto = (objeto) =>{
     //  for in que recorre el objeto, mostrando el clave-valor de cada atributo del objeto que se pase como parámetro
+    console.log(".......Mostrando Objeto........")
     for (let clave in objeto){
         console.log(clave+ ": "+objeto[clave]);
     };
+    console.log("...............................")
 }
 export {recorrerObjeto};
--- PracticasPabloV_PEC/Practica2.03-PabloV/ejercicio5.js
+++ PracticasPabloV_PEC/Practica2.03-PabloV/ejercicio5.js
@@ -1,28 +1,30 @@
 "use strict"
 //Ejercicio 5: Mostrando objetos II
 const recorrerObjetoII = (objeto) =>{
+    console.log(".......Mostrando ObjetosII........")
     for (let clave in objeto){
 
         if (typeof objeto[clave] == "string"){
-            console.log("Clave: "+clave +"Valor: "+objeto[clave] +"//-->Tipo de Dato: String.");
+            console.log(clave +" : "+objeto[clave] +" //-->Tipo de Dato: String.");
         }
         else if (typeof objeto[clave] == "number"){
-            console.log("Clave: "+clave +"Valor: "+objeto[clave].toLocaleString("es-ES") +"//-->Tipo de Dato: Número.");
+            console.log(clave +" : "+objeto[clave].toLocaleString("es-ES") +" //-->Tipo de Dato: Número.");
         }
         else if (Array.isArray(objeto[clave])){
-            console.log("Clave: "+clave +"Valor: "+objeto[clave] +"//-->Tipo de Dato: Array.");
+            console.log(clave +" : "+objeto[clave] +" //-->Tipo de Dato: Array.");
         }
         else if (typeof objeto[clave] == "function"){
-            console.log("Clave: "+clave +"Valor: "+objeto[clave]+"//-->Tipo de Dato: Función.");
+            console.log(clave +" : "+objeto[clave]+" //-->Tipo de Dato: Función.");
         }
         else if (typeof objeto[clave] == "object" && objeto[clave] != null){
-            console.log("Clave: "+clave +"Valor: "+objeto[clave] +"//-->Tipo de Dato: Objeto.");
+            console.log(clave +" : "+objeto[clave] +" //-->Tipo de Dato: Objeto.");
             recorrerObjetoII(objeto[clave]);
 
         }else{
-            console.log("Clave: "+clave+" Valor: "+objeto[clave]+"//-->Tipo de Dato: Otro.")
+            console.log(clave+" : "+objeto[clave]+" //-->Tipo de Dato: Otro.")
         }        
      
     }
+    console.log("................................")
 }
 export {recorrerObjetoII};
