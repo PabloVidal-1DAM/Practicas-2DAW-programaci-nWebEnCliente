7177b2212ca6cd0997edb982de0a7b0735095f69
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-09-29 17:53:35 +0200

    29/09/2025

--- PracticasPabloV_PEC/Practica3.04-PabloV/ejercicio1.js
+++ PracticasPabloV_PEC/Practica3.04-PabloV/ejercicio1.js
@@ -4,9 +4,18 @@ const mezclandoObjetos = (array) =>{
     let arrayMayusc = array.map(valor => valor.toUpperCase());
 
     arrayMayusc.forEach((valor,indice,array) => {
-        console.log(`Valor ${indice}: ${valor}`)
+        console.log(`Valor ${indice}: ${valor}`);
     });
+
     //punto 2:
+    // se hace una copia del array pasado por parámetro, se ordena son .sort() y se da la vuelta al orden con .reverse()
+    let arrayOrdenado = [...array].sort().reverse(); 
+
+    console.log("Array Ordenado Alfabeticamente y al revés:")
+    
+    arrayOrdenado.forEach((valor,indice,array) =>{
+        console.log(`Valor ${indice + 1}: ${valor}`);
+    });
 
 
 }
