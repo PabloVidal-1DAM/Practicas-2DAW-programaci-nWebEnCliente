5e7684807242606fd3beb4cf4dc78e5eba74d5c3
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-09-30 21:59:10 +0200

    30/09/2025

--- PracticasPabloV_PEC/Practica3.04-PabloV/ejercicio2.js
+++ PracticasPabloV_PEC/Practica3.04-PabloV/ejercicio2.js
@@ -9,7 +9,7 @@ const filtrandoObjetos = ()=>{
         array2[i] = generarNumAleatorio();
         array3[i] = generarNumAleatorio();
     }
-    // se hace una copia de todos los arrays anteriores
+    // se hace una copia de todos los arrays anteriores.
     let arraySumado = [...array1,...array2, ...array3];
 
    let arrayFinal = arraySumado.filter((valor,indice,array) =>{
@@ -18,6 +18,7 @@ const filtrandoObjetos = ()=>{
         );
     });
 
+    // ya que solo se va a recorrer para imprimirse, he optado por un forEach.
     console.log("Números Mayor o igual a 5:");
     arrayFinal.forEach((valor,indice,array) =>{
         console.log(`${valor}`)
@@ -26,7 +27,7 @@ const filtrandoObjetos = ()=>{
 
 const generarNumAleatorio = ()=>{
     // Math.random genera un número que va del 0 al 9, y el +1 que hay fuera aumenta el rango del mínimo (del 1 al 10)
-    // por otro lado, Math.floor redondea el resultado hacía arriba o hacía abajo para no generar números de tipo double.
+    // Por otro lado, Math.floor redondea el resultado hacía arriba o hacía abajo para no generar números de tipo double.
     return(Math.floor(Math.random() *10) +1 ); 
 }
 
