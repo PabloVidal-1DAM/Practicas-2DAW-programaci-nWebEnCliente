bc6b57e7e6ff01b715f21ced4035edc82a32df48
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-10-30 19:07:38 +0100

    30/10/2025

--- PracticasPabloV_PEC/Practica5.02-PabloV/mainEj2.js
+++ PracticasPabloV_PEC/Practica5.02-PabloV/mainEj2.js
@@ -1,6 +1,14 @@
 "use strict";
+let botones = document.getElementsByTagName("button");
 let contenedores = document.getElementsByTagName("div");
 let contenido = document.querySelectorAll("h1, p")
 
+const mostrarContenido = () =>{
+    
+}
+
+botones.item(0).addEventListener("click", () =>mostrarContenido(), false);
+
+console.log(botones);
 console.log(contenedores);
 console.log(contenido);
