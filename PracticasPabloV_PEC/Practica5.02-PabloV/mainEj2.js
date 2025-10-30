f6f5d40d8d1cce3e60e43eaf29629820ef8d4af2
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-10-30 20:47:26 +0100

    30/10/2025

--- PracticasPabloV_PEC/Practica5.02-PabloV/mainEj2.js
+++ PracticasPabloV_PEC/Practica5.02-PabloV/mainEj2.js
@@ -3,11 +3,12 @@ let botones = document.getElementsByTagName("button");
 let contenedores = document.getElementsByTagName("div");
 let contenido = document.querySelectorAll("h1, p")
 
-const mostrarContenido = () =>{
-    
+const mostrarContenido = (posicionTitulo, posicionTexto) =>{
+    contenido.item(posicionTitulo).classList.toggle("oculto");
+    contenido.item(posicionTexto).classList.toggle("oculto");
 }
 
-botones.item(0).addEventListener("click", () =>mostrarContenido(), false);
+botones.item(0).addEventListener("click", () =>mostrarContenido(0,1), false);
 
 console.log(botones);
 console.log(contenedores);
