1c095d6066c0febf89f4f15308cbbbfa11c6c1e4
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-11-09 19:17:20 +0100

    09/11/2025

--- PracticasPabloV_PEC/Practica5.04-PabloV/biblioteca.js
+++ PracticasPabloV_PEC/Practica5.04-PabloV/biblioteca.js
@@ -1,56 +1,99 @@
 "use strict";
 const imagenes = [
-    "imagenes/fila-1-columna-1.jpg",
-    "imagenes/fila-1-columna-2.jpg",
-    "imagenes/fila-1-columna-3.jpg",
-    "imagenes/fila-2-columna-1.jpg",
-    "imagenes/fila-2-columna-2.jpg",
-    "imagenes/fila-2-columna-3.jpg",
-    "imagenes/fila-3-columna-1.jpg",
-    "imagenes/fila-3-columna-2.jpg",
-    "imagenes/fila-3-columna-3.jpg"
+  "imagenes/fila-1-columna-1.jpg",
+  "imagenes/fila-1-columna-2.jpg",
+  "imagenes/fila-1-columna-3.jpg",
+  "imagenes/fila-2-columna-1.jpg",
+  "imagenes/fila-2-columna-2.jpg",
+  "imagenes/fila-2-columna-3.jpg",
+  "imagenes/fila-3-columna-1.jpg",
+  "imagenes/fila-3-columna-2.jpg",
+  "imagenes/fila-3-columna-3.jpg",
 ];
 
-const crearPiezas = () =>{
-    const contenedorPiezasPuzzle = document.getElementById("contenedorPiezasPuzzle");
-    contenedorPiezasPuzzle.classList.add("zonaSoltar");
-    let arrayImg = [];
+const contenedorPanelJuego = document.getElementById("contendorPanelJuego");
+const contenedorPiezasPuzzle = document.getElementById("contenedorPiezasPuzzle");
 
-    for (let i = 0; i< imagenes.length; i++){
-        const img = document.createElement("img");
-        img.classList.add("img_css");
-        img.setAttribute("draggable", true);
-        img.setAttribute("id", i);
+const crearPiezas = () => {
+  contenedorPiezasPuzzle.classList.add("zonaSoltar");
+  let arrayImg = [];
 
-        img.src = imagenes[i];
-        arrayImg = [...arrayImg, img];
+  for (let i = 0; i < imagenes.length; i++) {
+    const img = document.createElement("img");
+    img.classList.add("img_css");
+    img.setAttribute("draggable", true);
+    img.setAttribute("id", i);
+
+    img.src = imagenes[i];
+    arrayImg = [...arrayImg, img];
+  }
+  const arrayImgRandomizado = arrayImg.sort(() => Math.random() - 0.5);
+
+  for (let i = 0; i < arrayImgRandomizado.length; i++) {
+    contenedorPiezasPuzzle.appendChild(arrayImgRandomizado[i]);
+  }
+};
+
+const crearPanelJuego = () => {
+  const tabla = document.createElement("table");
+  for (let i = 1; i <= 3; i++) {
+    const tr = document.createElement("tr");
+
+    for (let j = 1; j <= 3; j++) {
+      const td = document.createElement("td");
+      td.classList.add("estilosTabla");
+      td.classList.add("zonaSoltar");
+      tr.appendChild(td);
     }
-    const arrayImgRandomizado = arrayImg.sort(() => Math.random() - 0.5);
+    tabla.appendChild(tr);
+  }
+  contenedorPanelJuego.appendChild(tabla);
+};
 
-    for (let i = 0; i< arrayImgRandomizado.length; i++){
-        contenedorPiezasPuzzle.appendChild(arrayImgRandomizado[i]);
+const crearBotonReinicio = () => {
+  const contenedorBtnReinicio = document.getElementById("contenedorBotonReinicio");
+  const botonReinicio = document.createElement("button");
+  botonReinicio.textContent = "Reiniciar Puzzle";
+  botonReinicio.setAttribute("id", "btnReinicio");
+  botonReinicio.classList.add("btnReinicio_css");
+  contenedorBtnReinicio.appendChild(botonReinicio);
+};
+
+// Verificación de que el puzzle ha sido colocado correctamente:
+const verificarPuzzle = () => {
+  const imagenesPuzzle = contenedorPanelJuego.getElementsByTagName("img");
+  var puzzleCorrecto = true;
+
+  if (imagenesPuzzle.length === 9) {
+    for (let i = 0; i < imagenesPuzzle.length; i++) {
+      if (parseInt(imagenesPuzzle.item(i).getAttribute("id")) !== i) {
+        puzzleCorrecto = false;
+        break;
+      }
     }
-}
 
-const crearPanelJuego = () =>{
-    const contenedorPanelJuego = document.getElementById("contendorPanelJuego");
-
-    const tabla = document.createElement("table");
-    for( let i = 1; i <= 3; i++){
-        
-        const tr = document.createElement("tr");
-
-        for ( let j = 1; j <= 3; j++){
-            const td = document.createElement("td");
-            td.classList.add("estilosTabla");
-            td.classList.add("zonaSoltar");
-            tr.appendChild(td);
-        }
-        tabla.appendChild(tr);
-
+    if (puzzleCorrecto) {
+      const p = document.createElement("p");
+      p.textContent = "Puzzle Solucionado Correctamente";
+      p.classList.add("estiloTexto-DeVictoria");
+      p.setAttribute("id", "mensajePuzzleSolucionado");
+      document.body.appendChild(p);
     }
-    contenedorPanelJuego.appendChild(tabla);
-}
+  }
+};
 
-export {crearPiezas, crearPanelJuego};
+const reiniciarJuego = () => {
+  const mensaje = document.getElementById("mensajePuzzleSolucionado");
 
+  if (mensaje !== null) {
+    mensaje.remove();
+  }
+
+  contenedorPanelJuego.innerHTML = "";
+  contenedorPiezasPuzzle.innerHTML = "";
+
+  crearPiezas();
+  crearPanelJuego();
+};
+
+export { crearPiezas, crearPanelJuego, verificarPuzzle, crearBotonReinicio, reiniciarJuego };
--- PracticasPabloV_PEC/Practica5.04-PabloV/main.js
+++ PracticasPabloV_PEC/Practica5.04-PabloV/main.js
@@ -1,71 +1,95 @@
 "use strict";
-import {crearPiezas, crearPanelJuego} from "./biblioteca.js";
+import {
+  crearPiezas,
+  crearPanelJuego,
+  verificarPuzzle,
+  crearBotonReinicio,
+  reiniciarJuego
+} from "./biblioteca.js";
 
-window.onload = () =>{
-    crearPiezas();
-    crearPanelJuego();
+window.onload = () => {
+    // Se crean los elementos del juego: las piezas del puzzle, el campo de juego y el botón para reiniciar.
+  crearPiezas();
+  crearPanelJuego();
+  crearBotonReinicio();
 
-    const contenedorPanelJuego = document.getElementById("contendorPanelJuego");
-    const contenedorPiezasPuzzle = document.getElementById("contenedorPiezasPuzzle");
+  const contenedorPanelJuego = document.getElementById("contendorPanelJuego");
+  const contenedorPiezasPuzzle = document.getElementById("contenedorPiezasPuzzle");
+  const btnReinicio = document.getElementById("btnReinicio");
 
-    contenedorPiezasPuzzle.addEventListener("dragover", (evento) =>{
-        evento.preventDefault();
-    }, false)
+  // Se le asigan los eventos del para poder arrastrar y soltar al contenedor de las piezas del principio.
+  contenedorPiezasPuzzle.addEventListener(
+    "dragover",
+    (evento) => {
+      evento.preventDefault();
+    },
+    false
+  );
 
-    contenedorPiezasPuzzle.addEventListener("dragstart", (evento) =>{
-        evento.dataTransfer.setData("id", evento.target.id);
-    }, false)
+  contenedorPiezasPuzzle.addEventListener(
+    "dragstart",
+    (evento) => {
+        // Se hace uso del dataTransfer para almacenar en memoria el atributo id de la imagen que haga saltar el evento.
+      evento.dataTransfer.setData("id", evento.target.id);
+    },
+    false
+  );
 
-    contenedorPiezasPuzzle.addEventListener("drop", (evento) =>{
-        if (evento.target.classList.contains("zonaSoltar")){
-            const elementoSeleccionado = document.getElementById(evento.dataTransfer.getData("id"));
-            evento.target.appendChild(elementoSeleccionado);
-        }
-    }, false)
+  contenedorPiezasPuzzle.addEventListener(
+    "drop",
+    (evento) => {
+        // Unicamente se podrá soltar el elemento a una zona que tenga la clase "zonaSoltar"
+      if (evento.target.classList.contains("zonaSoltar")) {
+        const elementoSeleccionado = document.getElementById(
+            // Se accede al dataTransfer para seleccionar la imagen que ha hecho saltar el evento para insertarla en el contenedor.
+          evento.dataTransfer.getData("id")
+        );
+        evento.target.appendChild(elementoSeleccionado);
+      }
+    },
+    false
+  );
 
-    contenedorPanelJuego.addEventListener("dragover", (evento) =>{
-        evento.preventDefault();
-    }, false)
+  contenedorPanelJuego.addEventListener(
+    "dragover",
+    (evento) => {
+      evento.preventDefault();
+    },
+    false
+  );
 
-    contenedorPanelJuego.addEventListener("dragstart", (evento) =>{
-        evento.dataTransfer.setData("id", evento.target.id);
-    }, false)
+  contenedorPanelJuego.addEventListener(
+    "dragstart",
+    (evento) => {
+        // Uso de dataTransfer para guardar el id de la imgen que lo dispare.
+      evento.dataTransfer.setData("id", evento.target.id);
+    },
+    false
+  );
 
-    contenedorPanelJuego.addEventListener("drop", (evento) =>{
-        if (evento.target.classList.contains("zonaSoltar") && !evento.target.hasChildNodes()){
-            const elementoSeleccionado = document.getElementById(evento.dataTransfer.getData("id"))
-            evento.target.appendChild(elementoSeleccionado);
-            verificarPuzzle();
-        }
-    }, false)
+  contenedorPanelJuego.addEventListener(
+    "drop",
+    (evento) => {
+      if (
+        evento.target.classList.contains("zonaSoltar") &&
+        !evento.target.hasChildNodes()
+      ) {
+        const elementoSeleccionado = document.getElementById(
+          evento.dataTransfer.getData("id")
+        );
+        evento.target.appendChild(elementoSeleccionado);
+        verificarPuzzle();
+      }
+    },
+    false
+  );
 
-    // Uso de eventos de drag SOLO para aplicar estilos visuales con clases CSS:
+  btnReinicio.addEventListener(
+    "click",
+    (evento) => {
+        reiniciarJuego();
+    },
+    false
+  );
 
-    // TODO:
-
-    // Verificación de que el puzzle ha sido colocado correctamente:
-    const verificarPuzzle = () =>{
-        const imagenesPuzzle = contenedorPanelJuego.getElementsByTagName("img");
-        var puzzleCorrecto = true;
-
-        if (imagenesPuzzle.length === 9){
-            
-            for (let i = 0; i< imagenesPuzzle.length; i++){
-                if(parseInt(imagenesPuzzle.item(i).getAttribute("id")) !== i){
-                    puzzleCorrecto = false;
-                    break;
-                }
-            }
-
-            if (puzzleCorrecto){
-                const p = document.createElement("p");
-                p.textContent = "Puzzle Solucionado Correctamente";
-                p.classList.add("estiloTexto-DeVictoria");
-                document.body.appendChild(p);
-            }
-        }
-    }
-
-
-}// fin del window onload.
-
+}; // fin del window onload.
