cc817b370990b12bc23f7c091d6643ca36744ef1
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-11-13 18:57:19 +0100

    13/11/2025

--- PracticasPabloV_PEC/Practica5.05-PabloV/biblioteca.js
+++ PracticasPabloV_PEC/Practica5.05-PabloV/biblioteca.js
@@ -1,1 +1,20 @@
 "use strict";
+// Valida que el campo tenga al menos cinco caracteres y que sea obligatorio
+const validarCampoTexto = () =>{
+
+}
+
+// Valida que el campo tenga cuatro caracteres númericos.
+const validarFecha = () =>{
+
+}
+
+// Valida que se ha seleccionado alguna de las opciones del radio buton.
+const validarTipoMusica = () =>{
+
+}
+
+// Valida que se usa el formato de dos letras mayÚsculas (ES-001AA)
+const validarLocalizacion = () =>{
+
+}
