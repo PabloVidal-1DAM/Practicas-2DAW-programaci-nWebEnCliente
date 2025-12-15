686e2d2e0f3ba3eb1059b0de2b197235eaa046af
PabloVidal-1DAM <pabloskyorito@gmail.com>
2025-12-15 12:35:27 +0100

    15/12/2025

--- PracticasPabloV_PEC/practicaExamen1Evaluacion/biblioteca.js
+++ PracticasPabloV_PEC/practicaExamen1Evaluacion/biblioteca.js
@@ -1,5 +1,64 @@
 "use strict";
 
-const traerDatos = (url) =>{
-    
-}
+const traerDatos = async (url) => {
+  try {
+        const respuesta = await fetch(url);
+
+        // si algo sale mal
+        if (!respuesta.ok) {
+            throw new Error(`Ha ocurrido un error al traer los datos de la API: ${respuesta.status} : ${respuesta.statusText}`);
+        }
+
+        const datos = await respuesta.json();
+
+        if(datos.results){
+            return datos.results;
+        }
+
+        return datos;
+    } catch (error) {
+        throw error;
+    }
+};
+
+    const traerInterpretes = async () =>{
+        const endpoints = await traerDatos("https://swapi.py4e.com/api/");
+
+        const datos = await traerDatos(endpoints.people);
+        return datos;
+    }
+
+    const insertarMensajeError = (contenedor, mensaje) =>{
+        contenedor.classList.remove("ocultar");
+
+        const listaErrores = document.getElementById("listaErrores");
+
+        if (!listaErrores){
+            mensaje = `<h3>Han ocurrido errores</h3>
+            <ol id="listaErrores">
+                <li><p>${mensaje}</p></li>
+            </ol>`;
+            contenedor.innerHTML = mensaje;
+        }else{
+            mensaje = `<li><p>${mensaje}</p></li>`;
+            listaErrores.innerHTML += mensaje;
+        }
+    }
+
+    const construirObjeto = (dato) =>{
+        const objeto = {
+            id: crypto.randomUUID(),
+            nombre: dato.name,
+            height: dato.height,
+            mass: dato.mass,
+            hair_color: dato.hair_color,
+            skin_color: dato.skin_color,
+            eye_color: dato.eye_color,
+            birth_year: dato.birth_year,
+            gender: dato.gender,
+            films: dato.films
+        }
+        return objeto
+    }
+
+export {traerDatos, traerInterpretes, insertarMensajeError, construirObjeto};
--- PracticasPabloV_PEC/practicaExamen1Evaluacion/main.js
+++ PracticasPabloV_PEC/practicaExamen1Evaluacion/main.js
@@ -1,13 +1,30 @@
 "use strict";
-window.onload = () =>{
-    const contenedorErrores = document.getElementById("contenedorErrores");
-    const formulario = document.getElementById("formularioPersonaje")
+import { traerDatos, traerInterpretes, insertarMensajeError, construirObjeto } from "./biblioteca.js";
+window.onload = () => {
+  const contenedorErrores = document.getElementById("contenedorErrores");
+  const formulario = document.getElementById("formularioPersonaje");
 
-    formulario.addEventListener("click", (evento) =>{
-        if(evento.target.id === "btnEnviar"){
-            evento.preventDefault();
-        }
-    }, false)
+  let objetoJSON = [];
 
-    
-}
+  formulario.addEventListener(
+    "click",
+    (evento) => {
+      if (evento.target.id === "btnEnviar") {
+        evento.preventDefault();
+        console.log(objetoJSON)
+      }
+    },
+    false
+  );
+
+  const Interpretes = async() => {
+    try{
+        const datosInterpretes = await traerInterpretes();
+        objetoJSON = datosInterpretes.map((interprete) => construirObjeto(interprete));
+    }catch(error){
+        insertarMensajeError(contenedorErrores, error.message);
+    }
+  };
+
+  Interpretes();
+};
